d5568893eb6962e557ab93eee56b2d9e32aa91ff
Tzu-Yin Hong <lili668668@gmail.com>
2016-11-08 11:49:04 +0800

    新增sql server的連線

--- server.js
+++ server.js
@@ -3,14 +3,22 @@ var express = require('express');
 var socketio = require('socket.io');
 var bodyparser = require('body-parser');
 var cc = require('config-multipaas');
+var mssql = require('mssql');
 
 var app = express();
 var server = http.createServer(app);
 var io = socketio(server);
 var config = cc();
+var sqlConfig = {
+    user: process.env.npm_package_config_dbUser,
+    password: process.env.npm_package_config_dbPasswd,
+    server: process.env.npm_package_config_dbServer,
+    database: process.env.npm_package_config_dbName,
 
-var people_counter = 0;
-var name;
+    options: {
+        encrypt: true
+    }
+}
 
 app.use('/css', express.static('css'));
 app.use('/js', express.static('js'));
@@ -23,6 +31,14 @@ app.use( bodyparser.urlencoded({
 
 app.get('/', function(request,response){
     response.sendFile(__dirname + "/index.html");
+});
+
+mssql.connect(sqlConfig, function(err){
+    if (!err) {
+        console.log('sql connect success.');
+    } else {
+        console.log(err);
+    }
 });
 
 io.on('connection', function(socket){
