297208bfc6da2a05ed8050d1b5e9c7ff4b3ebaf6
Tzu-Yin Hong <lili668668@gmail.com>
2016-11-08 22:57:10 +0800

    我絕對不會說我寫密碼檢驗寫了一個小時

--- js/register.js
+++ js/register.js
@@ -1,6 +1,16 @@
-new Vue({
-    el: "#test",
-    data: {
-        message: ""
-    }
+$(function(){
+    $("#againPassword").keyup(function(){
+        var value = $( this ).val();
+        var password = $( "#password" ).val();
+        console.log(value);
+        console.log(password);
+        console.log(value != password);
+        if (value != password) {
+            $( "#checkPassword" ).css("display", "block");
+        } else {
+            $( "#checkPassword" ).css("display", "none");
+        }
+        
+    })
+    .keyup();
 });
