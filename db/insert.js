c22189b7d6dddf01a6e719ea0162b12f7edecd4a
Tzu-Yin Hong <lili668668@gmail.com>
2016-11-12 07:18:54 +0800

    fix insert

--- db/insert.js
+++ db/insert.js
@@ -1,66 +1,67 @@
 var config = require('./dbConfig.js');
 var mssql = require('mssql');
 
-const sMember = "MEMBER";
-const sProfessor = "PROFESSOR";
-const sTa = "TA";
-const sStudent = "STUDENT";
-const sOrderMan = "ORDER_MAN";
+const sMember = 'MEMBER';
+const sProfessor = 'PROFESSOR';
+const sTa = 'TA';
+const sStudent = 'STUDENT';
+const sOrderMan = 'ORDER_MAN';
 
 // memeber
-const sAccount = "Account";
-const sPassword = "Password";
-const sName = "Name";
-const sSsid = "SSID";
-const sEmail = "Email";
-const sType = "Status";
-const sQcount = "QCount";
+const sAccount = 'account';
+const sPassword = 'password';
+const sName = 'name';
+const sSsid = 'ssid';
+const sEmail = 'email';
+const sType = 'status';
+const sQcount = 'qcount';
+const initQcount = 0;
 
 // PROFESSOR
-const sProid = "proid";
-const sOffice = "office";
-const sGrade = "grade";
+const sProid = 'proid';
+const sOffice = 'office';
+const sGrade = 'grade';
 
 // TA
-const sTaid = "TAID";
-const sRoom = "room";
+const sTaid = 'taid';
+const sRoom = 'room';
 
 // STUDENT
-const sSID = "SID";
-const sClass = "class";
+const sSID = 'sid';
+const sClass = 'class';
 
 // ORDER_MAN
-const sOrderid = "OrderID";
+const sOrderManid = 'omid';
 
 function register_member(account, password, name, ssid, email, type, otherSet) {
 
-    var sql = `INSERT INTO ${sMember} ("${sAccount}", "${sPassword}", "${sName}", "${sSsid}", "${sEmail}", "${sType}", "${sQcount}") VALUES ('${account}', '${password}', '${name}', '${ssid}', '${email}', '${type}', 0);`;
+    var sql = `INSERT INTO ${sMember} (${sAccount}, ${sPassword}, ${sName}, ${sSsid}, ${sEmail}, ${sType}, ${sQcount}) VALUES ('${account}', '${password}', '${name}', '${ssid}', '${email}', '${type}', ${initQcount});`;
 
     set(sql);
 }
 
 function register_professor(account, password, name, ssid, email, type, proid, office, grade, otherSet) {
-    var sql = `insert into ${sProfessor}(${sAccount}, ${sProid}, ${sOffice}, ${sGrade}) values("${account}", "${proid}", "${office}", "${grade}")`;
+    var sql = `insert into ${sProfessor}(${sAccount}, ${sProid}, ${sOffice}, ${sGrade}) values('${account}', '${proid}', '${office}', '${grade}')`;
 
     register_member(account, password, name, ssid, email, type, set(sql, otherSet));
 }
 
 function register_ta(account, password, name, ssid, email, type, taid, room, otherSet) {
-    var sql = `insert into ${sTa}(${sAccount}, ${sTaid}, ${sRoom}) values("${account}", "${taid}", "${room}")`;
+    var sql = `insert into ${sTa}(${sAccount}, ${sTaid}, ${sRoom}) values('${account}', '${taid}', '${room}')`;
 
     register_member(account, password, name, ssid, email, type, set(sql, otherSet));
 
 }
 
 function register_student(account, password, name, ssid, email, type, sid, class_, otherSet) {
-    var sql = `insert into ${sStudent}(${sAccount}, ${sSID}, ${sClass}) values("${account}", "${sid}", "${class_}")`;
+    var sql = `insert into ${sStudent}(${sAccount}, ${sSID}, ${sClass}) values('${account}', '${sid}', '${class_}')`;
 
     register_member(account, password, name, ssid, email, type, set(sql, otherSet));
 }
 
-function register_orderMan(account, password, name, ssid, email, type, orderid, otherSet) {
-    var sql = `insert into ${sOrderMan}(${sAccount}, ${sOrderid}) values("${account}", "${orderid}")`;
-    register_member(account, password, name, ssid, email, type);
+function register_orderMan(account, password, name, ssid, email, type, ordermanid, otherSet) {
+    var sql = `insert into ${sOrderMan}(${sAccount}, ${sOrderManid}) values('${account}', '${ordermanid}')`;
+    register_member(account, password, name, ssid, email, type, set(sql, otherSet));
 }
 
 function set(sqlstr, callback) {
@@ -74,8 +75,6 @@ function set(sqlstr, callback) {
             }
 
             var mydb = new mssql.Request(transaction);
-
-            //console.log(mydb);
 
             mydb.query(sqlstr, function(err, recordset, affected){
                 if (err) {
@@ -92,16 +91,13 @@ function set(sqlstr, callback) {
                             return;
                         });
                     }
-                    console.log("commited");
+                    console.log('commited');
                 });
             });
 
         });
     });
 
-    //if (callback && callback.typeof === 'function') {
-        //callback();
-    //}
 }
 
 module.exports.register_professor = register_professor;
